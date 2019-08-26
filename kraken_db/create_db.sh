ed72003b0b86e9f5603207238dc71c4398b180c2
Rachel Colqhoun <rachel.colquhoun@ed.ac.uk>
2019-08-26 12:10:50 +0100

    update kraken database script to use both old bold database and new ncbi downloads

--- kraken_db/create_db.sh
+++ kraken_db/create_db.sh
@@ -1,5 +1,5 @@
 mkdir bold
-gunzip data/BOLD-Animalia-COI5p.fasta.gz
 kraken2-build --download-taxonomy --db bold    
-kraken2-build --add-to-library data/BOLD-Animalia-COI5p.fasta --db bold
+kraken2-build --add-to-library data/BOLD-Animalia-COI5p_old.fasta --db bold
+kraken2-build --add-to-library data/NCBI_Animalia_BOLD_COI_250819.fasta --db bold
 kraken2-build --build --db bold    
